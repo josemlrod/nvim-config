85258cef35256f9bcd2a4d13c2bc28261eb78ff1
josemlrod <mrjosemlrod@gmail.com>
2025-08-29 19:41:43 -0400

    use prettier instead of biome

--- init.lua
+++ init.lua
@@ -801,7 +801,7 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
-        typescriptreact = { 'biome' },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
